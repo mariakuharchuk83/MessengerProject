4e0b9a6ccfff96e192691528fc6fd7b24a17b315
mariaren_kuk <maria.kuharchuk83@gmail.com>
2021-11-10 20:34:14 +0200

    set up

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Марія Кухарчук on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
