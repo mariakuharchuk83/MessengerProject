13161d9e06f4e167dc2e9de911412d6fda037af4
mariaren_kuk <maria.kuharchuk83@gmail.com>
2021-11-11 17:36:10 +0200

    designed log in screen

--- Messenger/Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logo")
         imageView.contentMode = .scaleAspectFit
         return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Address..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Password..."
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = UIColor.CustomColors.lightPink
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -24,13 +72,26 @@ class LoginViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
         
         /// Add subviews
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.frame.size.width/4
-        imageView.frame = CGRect(x: (view.frame.size.width - size)/2, y: 55, width: size, height: size)
+        scrollView.frame = view.bounds
+        let size = scrollView.width/4
+        
+        imageView.frame = CGRect(x: (scrollView.width - size)/2, y: 35, width: size, height: size)
+        
+        emailField.frame = CGRect(x: 30, y: imageView.buttom+20, width: scrollView.width-60, height: 52)
+        
+        passwordField.frame = CGRect(x: 30, y: emailField.buttom+20, width: scrollView.width-60, height: 52)
+        
+        loginButton.frame = CGRect(x: 30, y: passwordField.buttom+20, width: scrollView.width-60, height: 52)
+        
     }
     
     
---
+++ Messenger/Messenger/Resources/Extensions.swift
@@ -0,0 +1,57 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by Марія Кухарчук on 11.11.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    
+    public var buttom: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
+
+
+
+extension UIColor {
+    
+    struct CustomColors{
+        static let lightPink = UIColor.init(netHex: 0xF68DA0)
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+
+}
